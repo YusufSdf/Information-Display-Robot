1a5a1a75ede4d913e192ce8868803a72fed2d7f2
Yusuf <yusufsedef0@gmail.com>
2025-06-12 22:31:16 +0300

    Update web_dataShow.py

--- web_dataShow.py
+++ web_dataShow.py
@@ -9,44 +9,47 @@ try:
     st.markdown("#It will display the information you enter as an image or dataframe(.csv or .json).")
 
     upload = st.file_uploader("upload file for data")
-    file_ext = os.path.splitext(upload.name)[1].lower()
+    if upload is not None:
+        file_ext = os.path.splitext(upload.name)[1].lower()
+        data = None
 
-    if file_ext == ".csv":
-        data = pd.read_csv(upload,encoding="utf-8")
-    elif file_ext == ".json":
-        data = pd.read_json(upload,encoding="utf-8")
+        if file_ext == ".csv":
+            data = pd.read_csv(upload,encoding="utf-8")
+        elif file_ext == ".json":
+            data = pd.read_json(upload,encoding="utf-8")
 
-    vote = st.selectbox("select mod",["Picture","Dataset"])
+        vote = st.selectbox("select mod",["Picture","Dataset"])
 
-    def columns_create():
-        columns = [] 
-        for i in data.columns:
-            columns.append(i)
-        option1 = st.selectbox("select column1",columns)
-        option2 = st.selectbox("select column2",columns)
-        return option1,option2
+        def columns_create():
+            columns = [] 
+            for i in data.columns:
+                columns.append(i)
+            option1 = st.selectbox("select column1",columns)
+            option2 = st.selectbox("select column2",columns)
+            return option1,option2
 
-    def dataPandas():
-        option1,option2 =  columns_create()
-        new_data = {
-            option1:data[option1],
-            option2:data[option2]
-        }
-        second_data = pd.DataFrame(data=new_data)
-        st.dataframe(second_data)
+        def dataPandas():
+            option1,option2 =  columns_create()
+            new_data = {
+                option1:data[option1],
+                option2:data[option2]
+            }
+            second_data = pd.DataFrame(data=new_data)
+            st.dataframe(second_data)
 
-    def image():  
-            option1, option2 = columns_create()
-            fig, ax = plt.subplots()
-            ax.scatter(data[option1], data[option2])
-            ax.set_xlabel(option1)
-            ax.set_ylabel(option2)
-            st.pyplot(fig)
+        def image():  
+                option1, option2 = columns_create()
+                fig, ax = plt.subplots()
+                ax.scatter(data[option1], data[option2])
+                ax.set_xlabel(option1)
+                ax.set_ylabel(option2)
+                st.pyplot(fig)
 
-    if vote == "Picture":
-        image()
-    elif vote == "Dataset":
-        dataPandas()
-
+        if vote == "Picture":
+            image()
+        elif vote == "Dataset":
+            dataPandas()
+    else:
+        st.info("upload data")
 except:
     st.title("upload file")
